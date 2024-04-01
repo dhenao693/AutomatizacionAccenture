90b8f78d58ae0f4c98e1b8608ccaab4eea042596
Daniel Henao <dhenao693@gmail.com>
2024-04-01 11:30:09 -0500

    creacion de step para validacion

--- src/test/java/co/com/accenture/mobile/swaglabs/stepdefinitions/LoginStepDefinition.java
+++ src/test/java/co/com/accenture/mobile/swaglabs/stepdefinitions/LoginStepDefinition.java
@@ -1,6 +1,8 @@
 package co.com.accenture.mobile.swaglabs.stepdefinitions;
 
 import co.com.accenture.mobile.swaglabs.tasks.Login;
+import io.cucumber.java.PendingException;
+import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 
 import java.util.List;
@@ -12,5 +14,10 @@ public class LoginStepDefinition {
     @When("login in app")
     public void loginInApp(List<Map<String, String>> users) {
         theActorInTheSpotlight().attemptsTo(Login.withTheUser(users));
+    }
+
+    @Then("see the correct username")
+    public void seeTheCorrectUsername() {
+        throw new PendingException();
     }
 }
